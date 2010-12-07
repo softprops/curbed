8e34b3ca55d85c9f3d285676df88a03bc1b86ee6
softprops <d.tangren@gmail.com>
2010-12-07 01:19:38 -0500

    uf version bump

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,4 +1,6 @@
 import sbt._
 class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val uf = "net.databinder" %% "unfiltered-server" % "0.1.4-SNAPSHOT"
+  val ufversion = "0.2.3"
+  val ufj = "net.databinder" %% "unfiltered-jetty" % ufversion
+  val ufs = "net.databinder" %% "unfiltered-filter" % ufversion
 }
--- src/main/scala/curbed/curbed.scala
+++ src/main/scala/curbed/curbed.scala
@@ -3,8 +3,6 @@ package curbed
 import unfiltered.request._
 import unfiltered.response._
 
-import javax.servlet.http.{HttpServletRequest => Req}
-
 /** cache interface for client request counter */
 trait Cache {
   def get(k: String): Option[Int]
@@ -21,17 +19,17 @@ trait HashCache extends Cache {
 /** responsible for making keys from requests */
 trait LockSmith {
   /** @return a string key for a request */
-  def key: Req => String
+  def key[T]: HttpRequest[T] => String
 }
 
 /** makes keys from client ips */
 trait IpKeyer extends LockSmith {
-  override def key = { _.getRemoteAddr }
+  def key[T] = _ match { case RemoteAddr(addr, _) => addr }
 }
 
 /** responsible making a full key varying depending the size of a time window */
 trait Window { self: LockSmith => 
-  def fullkey = { r: Req => (self.key(r) :: fmtNow(dateFmt) :: Nil).mkString(":") }
+  def fullkey[T] = { r: HttpRequest[T] => (self.key(r) :: fmtNow(dateFmt) :: Nil).mkString(":") }
   /** @return a date formate based on the size of the window */
   protected def dateFmt: String
   private def fmtNow(fmt: String) = new java.text.SimpleDateFormat(fmt).format(new java.util.Date)
@@ -47,10 +45,11 @@ trait DailyWindow extends Window { self: LockSmith =>
   override protected def dateFmt = "yyyy-MM-dd"
 }
 
+
 /** Throttles requests based on windows of time limiting maxRequests requests per client */
-class Throttle extends IpKeyer with HashCache with HourlyWindow with unfiltered.Plan {
+class Throttle extends IpKeyer with HashCache with HourlyWindow with unfiltered.filter.Plan {
   object Throttled {
-    def unapply(r: Req) = {
+    def unapply[T](r: HttpRequest[T]) = {
       val k = fullkey(r)
       val cnt = get(k) match {
         case Some(n) => n + 1
@@ -62,21 +61,22 @@ class Throttle extends IpKeyer with HashCache with HourlyWindow with unfiltered.
     }
   }
   
-  def filter = {
+  def intent = {
     case Throttled(k, n) => block(k, n)
     case _ => Pass
   }
   
-  def maxRequests = 10
+  /** client is allowed  maxRequests per window before being blocked */
+  val maxRequests: Int = 10
   
   def block(k: String, n: Int) = PlainTextContent ~> Forbidden
 }
 
 object Server {
   def main(args: Array[String]) {
-    unfiltered.server.Http(8080).filter(new Throttle with DailyWindow {
+    unfiltered.jetty.Http(8080).filter(new Throttle with DailyWindow {
       override def maxRequests = 20
-    }).filter(unfiltered.Planify{
+    }).filter(unfiltered.filter.Planify {
      case _ => ResponseString("ah ha") 
     }).run
   }
